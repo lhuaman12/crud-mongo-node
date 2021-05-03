1f0862301d84c8626f3b990609b9ac838655093e
luhua12 <luis.12.river@gmail.com>
2021-05-03 01:07:31 -0300

    bigchange

--- src/database.js
+++ src/database.js
@@ -1,15 +1,15 @@
 const mongoose = require('mongoose');
 const db = mongoose.connection;
-/*mongoose.connect('mongodb+srv://sertec:Futbol12@cluster0.42ewj.mongodb.net/sertec-database?retryWrites=true&w=majority',{
+mongoose.connect('mongodb+srv://sertec:Futbol12@cluster0.42ewj.mongodb.net/sertec-database?retryWrites=true&w=majority',{
     useCreateIndex:true,
     useNewUrlParser:true,
     useFindAndModify:false,
     useUnifiedTopology:true
 })
 .then(db=>console.log('DB is connected')).catch(err=>console.log(err));
-*/
-// Local test:
 
+// Local test:
+/*
 mongoose.connect('mongodb://localhost/clients-app',{
     useCreateIndex:true,
     useNewUrlParser:true,
@@ -20,3 +20,4 @@ db.on('error',console.error.bind(console,'Connection error'));
 db.once('open',()=>{
     console.log('Connected to the DB');
 });
+*/
